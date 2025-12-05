44d5af1e8bfb321a64ded5376a10baae60d712ab
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-05 21:06:20 +0000

    Teach the Go service to trust the Without this, the service will not acknowledge the "ID card" presented by other services.

--- overwhelming-minotaur/main.go
+++ overwhelming-minotaur/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("service: overwhelming-minotaur")
+	fmt.Println("service: overwhelming-minotaur starting...")
+	// read the Root CA certificate file from the disk
+	caCertFile, err := os.ReadFile("certs/ca.crt")
+	if err != nil {
+		log.Fatalf("Error reading CA certificate: %v", err)
+	}
+	// create a new certificate pool
+	caCertPool := x509.NewCertPool() // think of this like a trusted contact list
+	// add our Root CA to the pool
+	ok := caCertPool.AppendCertsFromPEM(caCertFile)
+	if !ok {
+		log.Fatalf("Failed to append CA certifcate to pool. Is the file a valid PEM?")
+	}
+	fmt.Println("Complete: Root CA loaded and trusted.")
 }
