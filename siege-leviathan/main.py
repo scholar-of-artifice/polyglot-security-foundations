4b1a682874812534f35a5cdb5f5e35d0da6f35c5
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-16 17:21:03 +0000

    use environment variables in python service

--- siege-leviathan/main.py
+++ siege-leviathan/main.py
@@ -1,11 +1,13 @@
 import ssl
 import httpx
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 # define where the Vault agent will place the certs
-BUNDLE_CERT = "/app/certs/siege-leviathan.pem"
+BUNDLE_CERT = os.environ.get(key="CERT_BUNDLE")
+TARGET_URL = os.environ.get(key="TARGET_URL")
 
 
 @app.get("/")
@@ -14,6 +16,9 @@ async def root():
     This endpoint triggers the interaction.
     It acts as a Client to the 'overwhelming-minotaur' service.
     """
+    # check that the environment variables load
+    if not BUNDLE_CERT or not TARGET_URL:
+        return {"error": "Configuration Error: CERT_BUNDLE or TARGET_URL is not set."}
     # create a secure SSL context
     ssl_context = ssl.create_default_context(
         purpose=ssl.Purpose.SERVER_AUTH,
@@ -26,14 +31,13 @@ async def root():
     )
     # define the start service URL
     # NOTE: this comes from the docker-compose
-    target_url = "https://overwhelming-minotaur:9000"
     # send the message defined as an example
     message = "I sent you a secret message *giggle*"
-    print(f"Attempting to contact {target_url}...")
+    print(f"Attempting to contact {TARGET_URL}...")
     try:
         # use httpx with the custom SSL context
         async with httpx.AsyncClient(verify=ssl_context) as client:
-            response = await client.post(target_url, content=message)
+            response = await client.post(TARGET_URL, content=message)
             # return the conversation log
             return {
                 "siege_leviathan_says": message,
