8aa901912a64ef61d27d0959b7c33d838ddd2727
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-18 14:28:54 +0000

    make a basic script for trying to reach out to service

---
+++ reckless-sleuth/cmd/reckless-sleuth/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	// get the target from the environment
+	targetURL := os.Getenv("TARGET_URL")
+	if targetURL == "" {
+		fmt.Println("Warning: TARGET_URL not set. defaulting to localhost...")
+		targetURL = "https://localhost:9090"
+	}
+	gmt.Printf("recless-sleuth starting. Targeting: %s\n", targetURL)
+	// configure a client that skips verifying the server's CA
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InescureSkipVerify: true
+		},
+	}
+	client := &http.Client{Transport: tr}
+	// enter the "simulation loop"
+	maxAttempts := 30
+	for i := 1; i <= maxAttempts; i++ {
+		fmt.Printf("[%d/%d]Attempting to access %s without a certificate...\n", i, maxAttempts, targetURL)
+		resp, err := client.Get(targetURL)
+		if err != nil {
+			// this is the correct path
+			fmt.Printf("connection rejected: %v\n", err)
+		} else {
+			// this path is when the lock is broken...
+			fmt.Printf("CRITICAL FAILURE: connection accepted %s\n", resp.Status)
+			resp.Body.Close()
+		}
+		if i < maxAttempts {
+			// wait before trying again
+			time.Sleep(5 * time.Second)
+		}
+	}
+	fmt.Println("Test run complete.")
+}
