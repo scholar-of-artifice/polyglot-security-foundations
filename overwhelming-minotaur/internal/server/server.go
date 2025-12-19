46aff6c7b97885e0e99dba9da2f2914b520cc6d4
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-19 21:23:02 +0000

    use the reloader to check for the status of a cert

--- overwhelming-minotaur/internal/server/server.go
+++ overwhelming-minotaur/internal/server/server.go
@@ -17,7 +17,6 @@ func New(cfg *config.Config) *http.Server {
 	if err != nil {
 		log.Fatalf("Error reading CA certificate from %s: %v", cfg.CertFile, err)
 	}
-
 	// create a new certificate pool
 	caCertPool := x509.NewCertPool() // think of this like a trusted contact list
 	// add our Root CA to the pool
@@ -26,20 +25,23 @@ func New(cfg *config.Config) *http.Server {
 		log.Fatalf("Failed to append CA certifcate to pool. Is the file a valid PEM?")
 	}
 	fmt.Println("Complete: Root CA loaded and trusted.")
-
-	// read the public certificate and the private key as a pair
-	cert, err := tls.LoadX509KeyPair(cfg.CertFile, cfg.KeyFile)
-	if err != nil {
-		log.Fatalf("Error loading certificate keypair: %v", err)
+	// initialize a certificate reloader with the paths from the config
+	reloader := &CertReloader{
+		certFile: cfg.CertFile,
+		keyFile:  cfg.KeyFile,
+	}
+	// perform an initial load to ensure the certificate is valid before use
+	if err := reloader.maybeReload(); err != nil {
+		log.Fatalf("Error loading initial certificate keypair: %v", err)
 	}
 	fmt.Println("Complete: Identity Keypair loaded.")
 
 	// configure TLS parameters
 	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		ClientCAs:    caCertPool,
-		ClientAuth:   tls.RequireAndVerifyClientCert, // <- this enforces mutual TLS
-		MinVersion:   tls.VersionTLS12,
+		GetCertificate: reloader.GetCertificate, // <- use GetCertificate hook
+		ClientCAs:      caCertPool,
+		ClientAuth:     tls.RequireAndVerifyClientCert, // <- this enforces mutual TLS
+		MinVersion:     tls.VersionTLS12,
 	}
 	fmt.Println("Complete: TLS parameters configured. (mTLS enforced)")
 
